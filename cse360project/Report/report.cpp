0de252a1ab102580c0d71361edd4c6c06cb02793
Sajid Anwar <sajidanwar94@gmail.com>
2014-04-26 03:50:47 -0700

    Add a table to the bottom of the reports

--- cse360project/Report/report.cpp
+++ cse360project/Report/report.cpp
@@ -17,6 +17,26 @@ QString Report::graphHtmlTemplate(QStringList headers, QList<QStringList> data)
 
     QString headerRow = headers.join(", ");
     QString dataRows = dataRowsList.join(", ");
+
+    QString trHeader;
+    foreach (QString header, headers)
+    {
+        trHeader += "<th>" + header + "</th>";
+    }
+
+    QString trData;
+    foreach (QStringList row, data)
+    {
+        trData += "<tr>";
+
+        foreach (QString value, row)
+        {
+            trData += "<td>" + value + "</td>";
+        }
+
+        trData += "</tr>";
+    }
+
     QString formatted(
            "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
            " <html xmlns=\"http://www.w3.org/1999/xhtml\">"
@@ -42,11 +62,16 @@ QString Report::graphHtmlTemplate(QStringList headers, QList<QStringList> data)
            "  </head>"
            "  <body style=\"font-family: Arial;border: 0 none;\">"
            "    <div id=\"visualization\" style=\"margin: 0 auto; width: 500px; height: 400px; border: 1px solid #bbb; background: white;\"></div>"
+           ""
+           "    <table style=\"width: 500px; margin: 20px auto; font-size: 13px; border: 1px solid #ccc;\">"
+           "      <tr style=\"border-bottom: 1px solid #ccc;\">%3</tr>"
+           "      %4"
+           "    </table>"
            "  </body>"
            "</html>​"
     );
 
-    return formatted.arg(headerRow, dataRows);
+    return formatted.arg(headerRow, dataRows, trHeader, trData);
 }
 
 QString Report::notEnoughDataTemplate()
